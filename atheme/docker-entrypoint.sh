5a2a4bd78926a04b8caeb47669e270f8e010e2e6
Logan Honeycutt <admin@kzn.sh>
2025-09-10 03:16:09 -0400

    feat(atheme): add Docker support with Containerfile and entrypoint script
    This commit introduces a Docker setup for Atheme services, including a new Containerfile for building the application and a docker-entrypoint.sh script for runtime configuration. The Containerfile defines the build process using Alpine Linux, installs necessary dependencies, and sets up the Atheme environment. The entrypoint script ensures proper permissions and checks for writable directories before starting the services. Additionally, example configuration files are added to facilitate user setup and customization.

---
+++ atheme/docker-entrypoint.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+DATADIR=/usr/local/atheme/etc
+if ! test -w "$DATADIR/"; then
+    echo "ERROR: $DATADIR must be mounted to a directory writable by UID 1001"
+    exit 1
+fi
+
+DBPATH="$DATADIR/services.db"
+if test -f "$DBPATH" && ! test -r "$DBPATH"; then
+    echo "ERROR: $DBPATH must be readable by UID 1001"
+    exit 1
+fi
+
+TMPPATH="$DATADIR/services.db.new"
+if test -f "$TMPPATH" && ! test -w "$TMPPATH"; then
+    echo "ERROR: $TMPPATH must either not exist or be writable by UID 1001"
+    exit 1
+fi
+
+rm -f /usr/local/atheme/var/atheme.pid
+/usr/local/atheme/bin/atheme-services -n -c /usr/local/atheme/etc/atheme.conf "$@"
